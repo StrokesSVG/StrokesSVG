675d3861f223f1eb04f8a9a0c1e7bf33ba47db59
StrokesSVG <strokessvg@mailinator.net>
2015-03-20 13:34:56 +0800

    Create index gernerator

---
+++ generateIndex.js
@@ -0,0 +1,38 @@
+var fs = require('fs');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('strokes.db');
+
+var ids = [];
+ 
+db.each("SELECT code_point FROM strokes GROUP BY code_point", function(err, row) {
+	ids.push(row.code_point);
+}, function(err, numRows){
+	
+	var imgs = ids.map(function(code_point){
+		var c = String.fromCharCode(code_point);
+		return '<img src="'+c+'.svg" width="270" height="270" />';
+	});
+	
+	var i = 0;
+	while(imgs.length){
+		var slice = imgs.splice(0, 50);
+		
+		(function(i,slice){
+			fs.open("svg/index"+i+".html", "w", function(err, fd){
+				if(err){
+					console.log(err);
+					return;
+				}
+				fs.write(fd, slice.join(" "), function(err, written, buffer){
+					console.log("Written " + i);
+					fs.close(fd);
+				});
+				
+			});
+		}(i, slice));
+		
+		i++;
+	}
+	
+	db.close();
+});
